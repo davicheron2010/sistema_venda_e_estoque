d04d20024054180019b8bb9a2a329d43c33be4ed
erykseconelli <wjeronimo22@gmail.com>
2026-04-16 22:46:34 -0300

    Update purchase.js
    update purchase

--- app/view/pages/assets/js/purchase.js
+++ app/view/pages/assets/js/purchase.js
@@ -1,19 +1,19 @@
 $('#fornecedor_id').select2({
     theme: 'bootstrap-5',
-    placeholder: "Selecione um produto",
+    placeholder: "Selecione um fornecedor",
     language: "pt-BR",
     ajax: {
         transport: async function (params, success, failure) {
             try {
                 const searchTerm = params.data.q || '';
 
-                const result = await window.api.product.find({ q: searchTerm });
+                const result = await window.api.supplier.find({ q: searchTerm });
 
                 // Adapta para o formato esperado pelo Select2
                 success({
                     results: result.data.map(item => ({
                         id: item.id,
-                        text: 'Cód: ' + item.id + ' - ' + item.nome + ' Cód. barra: ' + item.codigo_barra
+                        text: item.nome
                     }))
                 });
 
